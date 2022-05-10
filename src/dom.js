dd5eaa77856a4af2cb2f6fb5c01e625fa926a98c
liuyu-22 <63578680+liuyu-22@users.noreply.github.com>
2022-05-10 16:01:20 +0800

    Update dom.js

--- src/dom.js
+++ src/dom.js
@@ -125,3 +125,27 @@ window.dom = {
     return i;
   },
 };
+
+window.dom = {
+   find(selector, scope) {
+    return (scope || document).querySeletorAll(selector);
+  },
+  style(node, name, value) {
+    if (arguments.length === 3) {
+      node.style[name] = value;
+    } else if (arguments.length === 2) {
+      if (typeof name === "string") {
+        return node.style[name];
+      } else if (name instanceof Object) {
+        const Object = name;
+        for (let key in object) {
+          node.style[key] = object[key];
+        }
+      }
+    }
+  },
+  each(nodeList, fn) {
+    for (let i = 0; i < nodeList.length; i++) {
+      fn.call(null, nodeList[i]);
+    }
+  }
